03fb25f52d4d59011f474d14e51791f7aea4f4ef
jippysquirmwood <jippysquirmwood@gmail.com>
2019-08-08 08:59:28 +0100

    unchanged

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './UserInput.css';
 
 const UserInput = (props) => {
+
+
   return (
     <div className="UserInput">
       <input type="text" onChange={props.changed} value={props.username}/>
